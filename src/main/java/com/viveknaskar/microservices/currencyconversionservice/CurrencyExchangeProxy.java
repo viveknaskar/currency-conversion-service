442d780a9a53f256d3cdcd6c6d7dd110cf859aa9
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-11 03:22:22 +0530

    Changes for facilitating load-balancing with feign, eureka server and spring cloud LoadBalancer

--- src/main/java/com/viveknaskar/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/viveknaskar/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
  * FeignClient annotation is used to consume REST API endpoint.
  * Here name is the application name to be consumed and
  * url is the rest endpoint
+ * **************************************************
+ * @FeignClient(name = "currency-exchange", url = "localhost:8000")
  */
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+
+/**
+ * Just adding the application name to be consumed is enough in the FeignClient
+ * as the multiple instances of the consumed rest api will be load balanced
+ * by the eureka server
+ */
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
