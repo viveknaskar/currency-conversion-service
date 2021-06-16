b39cf2c8b070a43ff4f6e59463e9c43022e4fe00
Vivek Naskar <viveknaskar2912@gmail.com>
2021-06-17 03:10:42 +0530

    Added the microservice to docker-compose.yaml with required pom changes

--- src/main/java/com/viveknaskar/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/viveknaskar/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -41,7 +41,7 @@ public class CurrencyConversionController {
         uriVariables.put("to", to);
         
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
-                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                .getForEntity("http://currency-exchange:8000/currency-exchange/from/{from}/to/{to}",
                 CurrencyConversion.class, uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
